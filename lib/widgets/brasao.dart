1597569913dd5570b0eab5212b24e6bebc9ec283
Wallisson Martins <wallissonmartins37@gmail.com>
2023-12-28 00:24:09 -0300

    change boxfit image

--- lib/widgets/brasao.dart
+++ lib/widgets/brasao.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 class Brasao extends StatelessWidget {
   final String? image;
   final double? width;
-  
-  const Brasao({Key? key,  this.width, required this.image}) : super(key: key);
+
+  const Brasao({Key? key, this.width, required this.image}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width,
-      child: Hero(tag: image!, child: Image.network(image!, fit: BoxFit.cover))
-    );
+        width: width,
+        child: Hero(
+          tag: image!,
+          child: Image.network(image!, fit: BoxFit.contain),
+        ));
   }
-  
 }
